3dc1b4dcfc6ae240e3587d6a904b7c6e23b1f951
Murt15 <murtazat941@gmail.com>
2022-11-04 11:48:00 +0530

    Req and response from server

---
+++ Node js/task7.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/home") {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>')
+        res.end();
+    }
+
+
+    else if (req.url == "/about") {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body><h1> Welcome to About Us page</h1></body>');
+        res.write('</html>')
+        res.end();
+    }
+
+    
+    else if (req.url == "/node") {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>')
+        res.end();
+    }
+
+});
+
+server.listen(4000);
