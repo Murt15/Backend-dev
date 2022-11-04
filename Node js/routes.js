6547f831d55db681ce9d622e903e3d6c57e100a6
Murt15 <murtazat941@gmail.com>
2022-11-04 17:57:53 +0530

    3rd party package

--- Node js/routes.js
+++ Node js/routes.js
@@ -39,6 +39,7 @@ const requestHandler=(req,res)=>{
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
                 return res.end();
+                
             });
             
     
