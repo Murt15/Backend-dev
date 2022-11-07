08222d8e900be4c65b9f02a3b0dfa87a39a279bb
Murt15 <murtazat941@gmail.com>
2022-11-07 15:58:14 +0530

    task2

--- Node js/app.js
+++ Node js/app.js
@@ -1,7 +1,16 @@
-const http = require('http');
+const express=require('express');
+
+const app=express();
+
+app.use((req,res,next) => {
+    console.log('In the Middleware')
+    next();
+});
+
+app.use((req,res,next) => {
+    console.log('In another Middleware')
+    res.send('<h1>Hello</h1>')
+});
 
 
-
-const server = http.createServer();
-
-server.listen(4000);
+app.listen(4000);
