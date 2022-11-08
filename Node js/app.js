eaffc605dfc40a22784c1258d2f5ee4784ec1428
Murt15 <murtazat941@gmail.com>
2022-11-08 13:32:33 +0530

    Adding Contact us in Nav Bar

--- Node js/app.js
+++ Node js/app.js
@@ -1,18 +1,22 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const path=require('path');
 
 const app=express();
 
 const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
+const contactRoutes=require('./routes/contactus')
 
-app.use(bodyParser.urlencoded({extende : false}));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(contactRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 app.listen(4000);
