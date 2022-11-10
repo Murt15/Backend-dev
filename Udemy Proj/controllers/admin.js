3234a5b1b8cc6736db76dd2566f3eb34f58d850c
Murt15 <murtazat941@gmail.com>
2022-11-10 12:37:52 +0530

    Adding database with sql

--- Udemy Proj/controllers/admin.js
+++ Udemy Proj/controllers/admin.js
@@ -14,8 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+  .then(()=>{
+    res.redirect('/');
+  })
+  .catch((err)=>console.log(err));
+  
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -56,16 +60,23 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProducts=(req,res,next)=>{
    const prodId = req.body.productId;
-  Product.deleteProductById(prodId);
-  res.redirect('/admin/products')
+  Product.deleteById(prodId)
+  .then(()=>{
+     res.redirect('/admin/products')
+  })
+  .catch(err=>console.log(err));
+ 
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows,fieldData])=>{
     res.render('admin/products', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  }).catch(err=>console.log(err))
+
+  
 };
