eadcf29d3f1fff9764a1d0d98febfd37b03b43d7
Murt15 <murtazat941@gmail.com>
2022-11-10 14:08:22 +0530

    Sequelize

--- Udemy Proj/util/database.js
+++ Udemy Proj/util/database.js
@@ -1,10 +1,8 @@
-const mysql=require('mysql2');
+const Sequelize=require('sequelize');
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'nodejs',
-    password:'murt1234'
-})
+const sequelize=new Sequelize('nodejs','root','murt1234',{
+    dialect:'mysql',
+    host:'localhost'});
 
-module.exports=pool.promise();
+
+module.exports=sequelize;    
